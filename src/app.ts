410fd92a0130a6fd1136c8b54c1771da9d008fbb
QuerenHope <querenhope@hotmail.com>
2022-10-31 20:40:18 -0300

    feat: Criaçao das rotas de Usuario

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,11 @@ import 'reflect-metadata';
 
 import express from 'express';
 import handleErrorMiddleware from './middlewares/handleError.middleware';
+import { userRouter } from './routes/Users.routes';
 
 const app = express();
+
+app.use('/users', userRouter)
 
 app.use(handleErrorMiddleware);
 
---
+++ src/routes/Users.routes.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+export const userRouter = Router()
+
+userRouter.post('',)
+userRouter.get('',)
+userRouter.delete('',)
