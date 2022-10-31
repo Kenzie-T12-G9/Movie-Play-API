5e05cae96209e8c7a040dfba2dcdb48e11d4b71d
myke <myke.programacao@gmail.com>
2022-10-31 13:33:47 -0300

    CHORE: initialization of initial files like docker and errors configuration, jest, packge.json,

---
+++ src/middlewares/handleError.middleware.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from "express"
+import { AppError } from "../error/AppError"
+
+const handleErrorMiddleware = ( error:Error, req:Request, res:Response, next:NextFunction ) => {
+  
+    if( error instanceof AppError  ){
+        return res.status(error.status).json({
+            message:error.message
+        })
+    }
+
+    return res.status(500).json({
+        message:"Internal error"
+    })
+}
+
+export default handleErrorMiddleware
