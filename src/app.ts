79e6bcf0804a02ba8313b7629913c2cb44f42b74
QuerenHope <querenhope@hotmail.com>
2022-10-31 21:09:54 -0300

    feat: Criaçao das rotas login

--- src/app.ts
+++ src/app.ts
@@ -7,16 +7,17 @@ import { ratingsRouter } from './routes/Ratings.routes';
 import { historyRouter } from './routes/History.routes';
 import { moviesRouter } from './routes/Movies.routes';
 import { seriesRouter } from './routes/Series.routes';
+import { sessionRouter } from './routes/Session.routes';
 
 const app = express();
 
 app.use('/users', userRouter)
+app.use('/login', sessionRouter)
+
 app.use('/ratings', ratingsRouter)
 app.use('/history', historyRouter)
 app.use('/movies', moviesRouter)
 app.use('/series', seriesRouter)
-
-
 
 app.use(handleErrorMiddleware);
 
