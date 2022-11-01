6bbff6fef259af7ce773c58250c925cdf2375f5a
myke <myke.programacao@gmail.com>
2022-11-01 15:48:12 -0300

    feat:init controller

--- src/middlewares/handleError.middleware.ts
+++ src/middlewares/handleError.middleware.ts
@@ -1,14 +1,14 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { AppError } from '../error/AppError';
 
-const handleErrorMiddleware = (error: Error, _: Request, res: Response) => {
+const handleErrorMiddleware = (error: Error, req: Request, res: Response, next:NextFunction) => {
   if (error instanceof AppError) {
-    return res.status(error.status).send({
+    return res.status(error.status).json({
       message: error.message,
     });
   }
 
-  return res.status(500).send({
+  return res.status(500).json({
     message: 'Internal error',
   });
 };
