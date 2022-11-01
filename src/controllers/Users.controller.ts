c7869978305614e3ed35d252a97d3e787ae1637c
QuerenHope <querenhope@hotmail.com>
2022-11-01 10:54:18 -0300

    FEAT: Criaçao CreateUserController e CreateUserServices

--- src/controllers/Users.controller.ts
+++ src/controllers/Users.controller.ts
@@ -1,14 +1,21 @@
-import { Request } from 'express';
+import { Request, Response } from 'express';
 // prettier-ignore
-import { IUserList, IUserRequest, IUserResponse, IUserUpdate } from '../interfaces/users';
+import { IUserList, IUserRequestBody, IUserResponse, IUserUpdate } from '../interfaces/users';
+import UsersService from '../services/Users.service';
 import service from '../services/Users.service';
 
 export default class UsersController {
-  static async create(req: IUserRequest, res: IUserResponse) {}
+  static async create(req: Request, res: Response) {
+
+    const createUser: IUserRequestBody = req.body
+    const newUser = await UsersService.create(createUser)
+    return res.status(201).json(newUser)
+    
+  }
 
   static read(_: Request, res: IUserList) {}
 
   static update(req: IUserUpdate, res: IUserResponse) {}
 
-  static delete(req: IUserRequest, res: Response) {}
+  static delete(req: IUserRequestBody, res: Response) {}
 }
--- src/services/Users.service.ts
+++ src/services/Users.service.ts
@@ -1,7 +1,41 @@
+import { hash } from 'bcryptjs'
+import AppDataSource from '../data-source';
+import { PaymentMethods } from '../entities/PaymentMethods.entity';
+import { Users } from '../entities/Users.entity';
+import { AppError } from '../error/AppError';
 import { IUserRequestBody } from '../interfaces/users';
 
 export default class UsersService {
-  static create({ name, email, password, isAdm }: IUserRequestBody) {}
+  static repository = AppDataSource.getRepository(Users)
+  static paymentRepo = AppDataSource.getRepository(PaymentMethods)
+
+  static async create({ name, email, password, paymentInfo }: IUserRequestBody) {
+
+    const emailAlreadyExists = await this.repository.findOneBy({email})
+
+    if(emailAlreadyExists){
+      throw new AppError('Email already exists 😭',400)
+    }
+
+    if(!password){
+      throw new AppError('Password is a required field',400)
+    }
+    const hashedPassword = await hash(password, 10)
+
+    const newPayment = this.paymentRepo.create(paymentInfo)
+    await this.paymentRepo.save(newPayment)
+
+    const newUser = this.repository.create({
+      name,
+      email,
+      password: hashedPassword,
+      PaymentMethods: newPayment
+    })
+
+    const user = await this.repository.save(newUser)
+
+    return user
+  }
 
   static read() {}
 
