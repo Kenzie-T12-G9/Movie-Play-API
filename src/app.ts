700c68af63efc8b5e52aaf6337dae1f6c39ce5c1
Francisco Stenico <francisco.stenico@gmail.com>
2022-11-02 02:43:49 -0300

    CHORE(documentation): /users & /sessions routes included in the documentation

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata';
-
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import documentation from './documentation/api-documentation.json';
+
 import handleErrorMiddleware from './middlewares/handleError.middleware';
 import { userRouter } from './routes/Users.routes';
 import { ratingsRouter } from './routes/Ratings.routes';
@@ -8,17 +10,20 @@ import { historyRouter } from './routes/History.routes';
 import { moviesRouter } from './routes/Movies.routes';
 import { seriesRouter } from './routes/Series.routes';
 import { sessionRouter } from './routes/Session.routes';
+import { authorsPage } from './documentation/authors';
 
 const app = express();
 app.use(express.json());
 
-app.use('/users', userRouter)
-app.use('/login', sessionRouter)
+app.use('/documentation', swaggerUi.serve, swaggerUi.setup(documentation));
+app.use('/authors', authorsPage);
 
-app.use('/ratings', ratingsRouter)
-app.use('/history', historyRouter)
-app.use('/movies', moviesRouter)
-app.use('/series', seriesRouter)
+app.use('/users', userRouter);
+app.use('/login', sessionRouter);
+app.use('/ratings', ratingsRouter);
+app.use('/history', historyRouter);
+app.use('/movies', moviesRouter);
+app.use('/series', seriesRouter);
 
 app.use(handleErrorMiddleware);
 
