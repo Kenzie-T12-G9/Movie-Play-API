c5ceea96b757e81310184277a38d79bd28a38358
myke <myke.programacao@gmail.com>
2022-11-02 00:35:52 -0300

    feat:functions series

---
+++ src/controllers/Series.controller.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from 'express';
+import service from '../services/Series.service';
+
+export default class SeriesController {
+  static async create(req: Request, res: Response) {
+
+    const data = req.body
+    const resData = await service.create( data )
+    return res.status(200).json(resData)
+  }
+
+  static async list(req: Request, res: Response) {
+
+    const resData = await service.list()
+    return res.status(200).json(resData)
+  }
+
+  static async update(req: Request, res: Response) {
+
+    const data = req.body
+    const { id } = req.params
+
+    const resData = await service.update( id, data )
+    return res.status(200).json(resData)
+  }
+
+  static async delete(req: Request, res: Response) {
+
+    const { id } = req.params
+
+    const resData = await service.delete( id )
+    return res.status(200).json(resData)
+  }
+
+  static async addEpisodeo(req: Request, res: Response) {
+
+    const data = req.body
+    const { id } = req.params
+
+    const resData = await service.addEpisodeo( id, data )
+    return res.status(200).json(resData)
+  }
+}
