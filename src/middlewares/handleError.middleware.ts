f32e6b9041cf8e14cda722160c6588f0135d7f8e
Francisco Stenico <francisco.stenico@gmail.com>
2022-11-02 03:03:32 -0300

    CHORE(error middleware): Switching paradigms (functional programming to OOP)

--- src/middlewares/handleError.middleware.ts
+++ src/middlewares/handleError.middleware.ts
@@ -1,17 +1,18 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import { AppError } from '../error/AppError';
 
-const handleErrorMiddleware = (error: Error, req: Request, res: Response, next:NextFunction) => {
-  
-  if (error instanceof AppError) {
-    return res.status(error.status).json({
-      message: error.message,
+class ErrorMiddleware {
+  static handler(error: Error, _: Request, res: Response) {
+    if (error instanceof AppError) {
+      return res.status(error.status).json({
+        message: error.message,
+      });
+    }
+
+    return res.status(500).json({
+      message: 'Internal error',
     });
   }
+}
 
-  return res.status(500).json({
-    message: 'Internal error',
-  });
-};
-
-export default handleErrorMiddleware;
+export default ErrorMiddleware;
