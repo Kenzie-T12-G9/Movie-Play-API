236ba3622cc0daa7508924cda2b9a30758257207
myke <myke.programacao@gmail.com>
2022-11-07 12:40:52 -0300

    FIX:putting the moles in an organized folder

---
+++ src/__tests__/mocks/session/index.ts
@@ -0,0 +1,28 @@
+export const createUserAdm = {
+    name: 'Francisco Stenico',
+    email: 'francisco@mail.com',
+    isAdm: true,
+    password: 'Teste123@',
+    paymentMethods: {
+      name: 'Francisco C Stenico',
+      cpf: '12345678900',
+      number: '1111222233334444',
+      dueDate: '2026-10-21',
+      code: '123',
+    },
+};
+
+export const userAdmLogin = {
+    email: 'francisco@mail.com',
+    password: 'Teste123@',
+}
+
+export const userAdmLoginIncorrectMail = {
+    email: "emailErrado@mail.com",
+    password: "Teste123@"
+}
+
+export const userAdmLoginIncorrectPassword = {
+    email: "emailErrado@mail.com",
+    password: "Teste123@"
+}
