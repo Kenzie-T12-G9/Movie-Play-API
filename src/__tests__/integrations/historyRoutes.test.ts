c9c4a5fb02ecf92e6d7eb9bffc0e6baf7040f337
alexandre <axdborges@outlook.com>
2022-11-08 17:01:25 -0300

    refactor: refactored history tests

--- src/__tests__/integrations/historyRoutes.test.ts
+++ src/__tests__/integrations/historyRoutes.test.ts
@@ -2,9 +2,16 @@ import { DataSource } from 'typeorm';
 import request from 'supertest';
 import AppDataSource from '../../data-source';
 import app from '../../app';
-import { } from '../mocks/session';
-import { createUserADM, createUserNotAdm, loginUserAdm, loginUserNotAdm } from '../mocks/Series/indes';
+import {} from '../mocks/session';
+import {
+  createSerie,
+  createUserADM,
+  createUserNotAdm,
+  loginUserAdm,
+  loginUserNotAdm,
+} from '../mocks/Series/indes';
 import { mockedMovieHistory, mockedSerieHistory } from '../mocks/History';
+import { createMovie } from '../mocks/Movies';
 
 describe('/history', () => {
   let connection: DataSource;
@@ -27,10 +34,17 @@ describe('/history', () => {
   });
 
   test('POST /history - should be able to create a movie history', async () => {
-    const loggedUser = await request(app).post('/login').send(loginUserAdm)
+    const loggedAdm = await request(app).post('/login').send(loginUserAdm);
+    await request(app)
+      .post('/movies')
+      .send(createMovie)
+      .set('Authorization', `Bearer ${loggedAdm.body.token}`);
+    const getMovie = await request(app).get('/movies');
+    mockedMovieHistory.userId = loggedAdm.body.id;
+    mockedMovieHistory.movieId = getMovie.body[0].id;
     const response = await request(app)
       .post('/history')
-      .set('Authorization', `Bearer ${loggedUser.body.token}`)
+      .set('Authorization', `Bearer ${loggedAdm.body.token}`)
       .send(mockedMovieHistory);
 
     expect(response.status).toBe(201);
@@ -41,10 +55,17 @@ describe('/history', () => {
   });
 
   test('POST /history - should be able to create a serie history', async () => {
-    const loggedUser = await request(app).post('/login').send(loginUserNotAdm)
+    const loggedAdm = await request(app).post('/login').send(loginUserAdm);
+    await request(app)
+      .post('/series')
+      .send(createSerie)
+      .set('Authorization', `Bearer ${loggedAdm.body.token}`);
+    const getSerie = await request(app).get('/series');
+    mockedSerieHistory.userId = loggedAdm.body.id;
+    mockedSerieHistory.seriesId = getSerie.body[0].id;
     const response = await request(app)
       .post('/history')
-      .set('Authorization', `Bearer ${loggedUser.body.token}`)
+      .set('Authorization', `Bearer ${loggedAdm.body.token}`)
       .send(mockedSerieHistory);
 
     expect(response.status).toBe(201);
@@ -55,40 +76,42 @@ describe('/history', () => {
   });
 
   test('POST /history - should not be able to create history without title ID', async () => {
-    const loggedUser = await request(app).post('/login').send(loginUserNotAdm)
+    const loggedAdm = await request(app).post('/login').send(loginUserAdm);
     const response = await request(app)
       .post('/history')
-      .set('Authorization', `Bearer ${loggedUser.body.token}`)
-      .send({ userId: '1127f3f2-0057-4d67-b68a-a689c3447331' });
+      .set('Authorization', `Bearer ${loggedAdm.body.token}`)
+      .send({ userId: `${loggedAdm.body.id}` });
 
     expect(response.status).toBe(400);
     expect(response.body).toHaveProperty('message');
   });
 
   test('POST /history - should not be able to create movie history without user ID', async () => {
-    const loggedUser = await request(app).post('/login').send(loginUserNotAdm)
+    const loggedAdm = await request(app).post('/login').send(loginUserAdm);
+    const getMovie = await request(app).get('/movies');
     const response = await request(app)
       .post('/history')
-      .set('Authorization', `Bearer ${loggedUser.body.token}`)
-      .send({ movieId: 'e0150833-89fc-436c-aa10-d2a68b0fa32f' });
+      .set('Authorization', `Bearer ${loggedAdm.body.token}`)
+      .send({ movieId: `${getMovie.body[0].id}` });
 
     expect(response.status).toBe(400);
     expect(response.body).toHaveProperty('message');
   });
 
   test('POST /history - should not be able to create series history without user ID', async () => {
-    const loggedUser = await request(app).post('/login').send(loginUserNotAdm)
+    const loggedAdm = await request(app).post('/login').send(loginUserAdm);
+    const getSerie = await request(app).get('/series');
     const response = await request(app)
       .post('/history')
-      .set('Authorization', `Bearer ${loggedUser.body.token}`)
-      .send({ seriesId: 'e0150833-89fc-436c-aa10-d2a68b0fa32f' });
+      .set('Authorization', `Bearer ${loggedAdm.body.token}`)
+      .send({ seriesId: `${getSerie.body[0].id}` });
 
     expect(response.status).toBe(400);
     expect(response.body).toHaveProperty('message');
   });
 
   test('POST /history - should not be able to create a history without authentication', async () => {
-    const loggedUser = await request(app).post('/login').send(loginUserNotAdm)
+    await request(app).post('/login').send(loginUserAdm);
     const response = await request(app)
       .post('/history')
       .set('Authorization', `Bearer ${null}`)
@@ -99,21 +122,21 @@ describe('/history', () => {
   });
 
   test('POST /history - should not be able to create a history with invalid user', async () => {
-    const loggedUser = await request(app).post('/login').send(loginUserNotAdm)
+    const loggedAdm = await request(app).post('/login').send(loginUserAdm);
     const response = await request(app)
-    .post('/history')
-    .set('Authorization', `Bearer ${loggedUser.body.token}`)
-    .send({
-      userId: undefined,
-      movieId: 'e0150833-89fc-436c-aa10-d2a68b0fa32f',
-    });
+      .post('/history')
+      .set('Authorization', `Bearer ${loggedAdm.body.token}`)
+      .send({
+        userId: undefined,
+        movieId: 'e0150833-89fc-436c-aa10-d2a68b0fa32f',
+      });
 
     expect(response.status).toBe(404);
     expect(response.body).toHaveProperty('message');
   });
 
   test('GET /history/movies - should be able to list all movies history', async () => {
-    const loggedAdm = await request(app).post('/login').send(loginUserAdm)
+    const loggedAdm = await request(app).post('/login').send(loginUserAdm);
     const response = await request(app)
       .get('/history/movies')
       .set('Authorization', `Bearer ${loggedAdm.body.token}`);
@@ -123,7 +146,7 @@ describe('/history', () => {
   });
 
   test('GET /history/movies - should not be able to list movies history without authentication', async () => {
-    const loggedAdm = await request(app).post('/login').send(loginUserAdm)
+    const loggedAdm = await request(app).post('/login').send(loginUserAdm);
     const response = await request(app).get('/history/movies');
 
     expect(response.status).toBe(401);
@@ -138,7 +161,7 @@ describe('/history', () => {
       .get('/history/movies')
       .set('Authorization', `Bearer ${userLoginResponse.body.token}`);
 
-    expect(response.status).toBe(403);
+    expect(response.status).toBe(401);
     expect(response.body).toHaveProperty('message');
   });
 
@@ -151,11 +174,11 @@ describe('/history', () => {
       .set('Authorization', `Bearer ${adminLoginResponse.body.token}`);
 
     expect(response.status).toBe(200);
-    expect(response.body).toHaveLength(2);
+    expect(response.body).toHaveLength(1);
   });
 
   test('GET /history/series - should not be able to list series history without authentication', async () => {
-    const loggedAdm = await request(app).post('/login').send(loginUserAdm)
+    const loggedAdm = await request(app).post('/login').send(loginUserAdm);
     const response = await request(app).get('/history/series');
 
     expect(response.status).toBe(401);
@@ -170,7 +193,7 @@ describe('/history', () => {
       .get('/history/series')
       .set('Authorization', `Bearer ${userLoginResponse.body.token}`);
 
-    expect(response.status).toBe(403);
+    expect(response.status).toBe(401);
     expect(response.body).toHaveProperty('message');
   });
 
@@ -191,7 +214,9 @@ describe('/history', () => {
     const adminLoginResponse = await request(app)
       .post('/login')
       .send(loginUserAdm);
-    const response = await request(app).get(`/history/movies/${mockedMovieHistory.movieId}`);
+    const response = await request(app).get(
+      `/history/movies/${mockedMovieHistory.movieId}`
+    );
 
     expect(response.status).toBe(401);
     expect(response.body).toHaveProperty('message');
@@ -226,7 +251,9 @@ describe('/history', () => {
     const adminLoginResponse = await request(app)
       .post('/login')
       .send(loginUserAdm);
-    const response = await request(app).get(`/history/series/${mockedSerieHistory.seriesId}`);
+    const response = await request(app).get(
+      `/history/series/${mockedSerieHistory.seriesId}`
+    );
 
     expect(response.status).toBe(401);
     expect(response.body).toHaveProperty('message');
